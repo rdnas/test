9a7ee2b670ad67f4b5289884d79e4bd3be513215
Alexander Zeikidze <alexander.zeikdze@crowdflower.com>
2016-03-08 19:04:25 +0200

    Updated function output message: TESTYEYEYEEYEYEYEYEY

--- src/main/java/example/Hello.java
+++ src/main/java/example/Hello.java
@@ -6,7 +6,7 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 public class Hello {
     public String myHandler(int myCount, Context context) {
         LambdaLogger logger = context.getLogger();
-        logger.log("TESTWOWOWOWOWOWOWOWOWWOOWWOOWWO Event received: " + myCount);
+        logger.log("TESTYEYEYEEYEYEYEYEYEYEYEEY Event received: " + myCount);
         return String.valueOf(myCount);
     }
 }
